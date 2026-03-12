82e9114343f7210fbcb9843776f5ac68192b4dab
Devashish <workfordevashishhaldar@gmail.com>
2026-03-12 23:46:02 +0530

    feat: add routing and main components for application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App() {
-
   return (
-    <div className=''>deva</div>
-  )
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <div>
+        <h1>Welcome to Tinder</h1>
+        <p>Discover new connections!</p>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
