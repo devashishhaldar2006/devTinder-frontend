f6157ad7e9e5327dfd687d16c56a3e6596ccdc8f
Devashish <workfordevashishhaldar@gmail.com>
2026-03-14 00:36:48 +0530

    feat: implement user fetching in Body component and update Navbar links to use React Router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = useSelector((state) => state.user);
@@ -31,16 +32,22 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
+              <Link to="/profile" className="justify-between">
                 Profile
                 <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a>Settings</a>
+              <Link to="/settings" className="justify-between">
+                Settings
+              </Link>
             </li>
             <li>
-              <a>Logout</a>
+              <Link to="/logout" 
+              
+              className="justify-between">
+                Logout
+              </Link>
             </li>
           </ul>
         </div>
